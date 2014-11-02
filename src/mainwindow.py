cc91b3532a88e08aad2474447b47e3dc031e05e7
Raphaël SEBAN <motus@laposte.net>
2014-11-02 12:09:33 +0100

    setting up project

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -31,6 +31,27 @@ class MainWindow (tkRAD.RADXMLMainWindow):
         Application's GUI main window
     """
 
+    def bind_events (self, **kw):
+        """
+            app-wide event bindings;
+        """
+        self.events.connect_dict(
+            {
+                "Project:New": self.slot_project_new,
+                "Project:Open": self.slot_project_open,
+                "Project:Save": self.slot_project_save,
+
+                "Edit:Undo": self.slot_edit_undo,
+                "Edit:Redo": self.slot_edit_redo,
+
+                "Tools:NameDatabase": self.slot_tools_name_db,
+
+                "Help:About": self.slot_about_dialog,
+            }
+        )
+    # end def
+
+
     def init_widget (self, **kw):
         """
             hook method to be reimplemented by subclass;
@@ -41,6 +62,64 @@ class MainWindow (tkRAD.RADXMLMainWindow):
         self.connect_statusbar("show_statusbar")
         # looks for ^/xml/widget/mainwindow.xml
         self.xml_build()
+        # event bindings
+        self.bind_events(**kw)
+    # end def
+
+
+    def slot_about_dialog (self, *args, **kw):
+        """
+            event handler for menu Help > About;
+        """
+        print(__name__)
+    # end def
+
+
+    def slot_edit_redo (self, *args, **kw):
+        """
+            event handler for menu Edit > Redo;
+        """
+        pass
+    # end def
+
+
+    def slot_edit_undo (self, *args, **kw):
+        """
+            event handler for menu Edit > Undo;
+        """
+        pass
+    # end def
+
+
+    def slot_project_new (self, *args, **kw):
+        """
+            event handler for menu Project > New;
+        """
+        pass
+    # end def
+
+
+    def slot_project_open (self, *args, **kw):
+        """
+            event handler for menu Project > Open;
+        """
+        pass
+    # end def
+
+
+    def slot_project_save (self, *args, **kw):
+        """
+            event handler for menu Project > Save;
+        """
+        pass
+    # end def
+
+
+    def slot_tools_name_db (self, *args, **kw):
+        """
+            event handler for menu Tools > Name database;
+        """
+        pass
     # end def
 
 # end class MainWindow

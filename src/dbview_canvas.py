50d57ab4e38e1d3d6e130dbf6a07b71e92301a07
Raphaël SEBAN <motus@laposte.net>
2014-11-19 10:47:28 +0100

    working on 'name database' dialog utility

--- src/dbview_canvas.py
+++ src/dbview_canvas.py
@@ -23,6 +23,7 @@
 """
 
 # lib imports
+from tkinter import ttk
 import tkRAD.core.async as ASYNC
 import tkRAD.widgets.rad_canvas as RC
 
@@ -38,6 +39,14 @@ class DBViewCanvas (RC.RADCanvas):
         "highlightbackground": "grey80",
         "highlightthickness": 1,
     } # end of CONFIG
+
+    CONFIG_BODY = {
+        "font": "monospace 10",
+    }
+
+    CONFIG_HEADER = {
+        "font": "monospace 11",
+    }
 
 
     def bind_events (self, **kw):
@@ -66,6 +75,18 @@ class DBViewCanvas (RC.RADCanvas):
     # end def
 
 
+    def ensure_visible_header (self, *args, **kw):
+        """
+            event handler: ensures header frame is always visible while
+            scrolling canvas;
+        """
+        # reset pos
+        self.coords(self.id_header, 0, self.canvasy(0))
+        # set to foreground
+        self.tag_raise(self.id_header, "all")
+    # end def
+
+
     def init_members (self, **kw):
         """
             class members only inits;
@@ -74,6 +95,17 @@ class DBViewCanvas (RC.RADCanvas):
         self.async = ASYNC.get_async_manager()
         self.field_sequence = None
         self.fields = dict()
+        self.column_index = 0
+        self.row_index = 0
+        # widget inits
+        self.frame_body = ttk.Frame(self)
+        self.frame_header = ttk.Frame(self)
+        self.id_body = self.create_window(
+            0, 0, window=self.frame_body
+        )
+        self.id_header = self.create_window(
+            0, 0, window=self.frame_header
+        )
     # end def
 
 
@@ -90,14 +122,46 @@ class DBViewCanvas (RC.RADCanvas):
     # end def
 
 
+    def insert_label (self, frame, **kw):
+        """
+            inserts a ttk.Label widget into @frame along with grid
+            internal (self.row_index, self.column_index) indices;
+        """
+        # inits
+        _label = ttk.Label(
+            frame,
+            text=_(kw.get("text") or "label"),          # i18n support
+            font=kw.get("font") or "monospace 10",
+            background=kw.get("background") or "white",
+            foreground=kw.get("foreground") or "black",
+        )
+        # insert into frame
+        _label.grid(row=self.row_index, column=self.column_index)
+        # next column
+        self.column_index += 1
+        # update canvas
+        self.update_canvas()
+    # end def
+
+
+    def next_row (self, *args, **kw):
+        """
+            event handler: sets indices for next row label insertion;
+        """
+        # inits
+        self.row_index += 1
+        self.column_index = 0
+    # end def
+
+
     def reset (self, *args, **kw):
         """
             event handler: resets all in widget;
         """
-        # reset members
-        self.init_members(**kw)
         # clear canvas
         self.clear_canvas(*args, **kw)
+        # reset members
+        self.init_members(**kw)
     # end def
 
 
@@ -110,7 +174,31 @@ class DBViewCanvas (RC.RADCanvas):
         self.reset()
         # update field names ordered sequence
         self.field_sequence = names
-        pass                                                                # FIXME
+        # browse names
+        for _name in names:
+            # set field options
+            _opts = self.set_field_options(_name, **options)
+            # set header label
+            self.insert_label(self.frame_header, text=_name, **_opts)
+        # end for
+        # reset column index
+        self.column_index = 0
+    # end def
+
+
+    def set_field_options (self, name, **options):
+        """
+            sets @options for a given field @name;
+        """
+        # inits
+        _options = self.fields.get(name) or self.CONFIG_HEADER.copy()
+        _options.update(options)
+        # last but not least
+        _options.update(name=name)
+        # set field options
+        self.fields[name] = _options
+        # return new options
+        return _options
     # end def
 
 

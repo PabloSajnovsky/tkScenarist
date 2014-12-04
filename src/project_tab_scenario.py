dd80d4f47a57d90be2e791eff16e40e364ceab06
Raphaël SEBAN <motus@laposte.net>
2014-12-04 12:40:43 +0100

    working on scenario tab features

--- src/project_tab_scenario.py
+++ src/project_tab_scenario.py
@@ -232,7 +232,7 @@ class ProjectTabScenario (tkRAD.RADXMLFrame):
         _word = self.TEXT.get_word()
         # look for matching names
         _names = self.tab_characters.get_matching_names(
-            _word["word"], self.TEXT.get_line_contents()
+            _word["word"], #self.TEXT.get_line_contents()
         )
         # got matching names?
         if _names:

a0fe4f97226a6b03d1f7f1b88be2f36658365171
Raphaël SEBAN <motus@laposte.net>
2014-12-13 12:42:17 +0100

    updated version number

--- tkscenarist.py
+++ tkscenarist.py
@@ -31,7 +31,7 @@ class tkScenarist (tkRAD.RADApplication):
     # class constant defs
     APP = {
         "name": _("tkScenarist"),
-        "version": _("0.6a"),
+        "version": _("0.7a"),
         "description": _("Movie scriptwriting utility program."),
         "title": _("tkScenarist - screen writing made simpler"),
         "author": _("Raphaël SEBAN <motus@laposte.net>"),

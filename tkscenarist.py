a3f65e4c799ae9e59b770d0893b6bef0c295f47e
Raphaël SEBAN <motus@laposte.net>
2015-01-18 18:37:35 +0100

    working on PDF export feature

--- tkscenarist.py
+++ tkscenarist.py
@@ -52,8 +52,8 @@ General Public License for more details.
 You should have received a copy of the GNU General Public License along
 with this program.
 
-If not, see: http://www.gnu.org/licenses/\
-        """),
+If not, see: http://www.gnu.org/licenses/
+        """).strip(),
         "license_url": _("http://www.gnu.org/licenses/"),
         "pdflib": "reportlab(tm)",
         "pdflib_author": "(c) ReportLab Europe Ltd. 2000-2012",
